74735858a338305fe26459a4b778742fb5ab026a
kiran-busibud <kiran@zinosi.com>
2023-05-05 14:54:18 +0530

    HELP-11630 #time 2h 15m new action event

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const new_email = require('./actions/new_email');
 const authentication = require('./authentication');
 const newProductTrigger = require('./triggers/new_product.js');
+const newEmailAction = require('./actions/new_email.js')
 
 module.exports = {
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
   authentication: authentication,
   triggers: { [newProductTrigger.key]: newProductTrigger },
+  actions: {[newEmailAction.key]: newEmailAction},
 };
